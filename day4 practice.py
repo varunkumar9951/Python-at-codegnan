54d19f23b34859fb2048924bda87f48b637f69e9
varunkumar9951 <yalapallivarunkumar@gmail.com>
2026-04-03 14:10:53 +0530

    Add files via upload

---
+++ day4 practice.py
@@ -0,0 +1,49 @@
+# 1st program
+num = int(input())
+if num == 0:
+    print("number is zero")
+else:
+    print("Non zero")
+
+#2nd program    
+num = int(input())
+if num<0:
+    print("negative")
+else:
+    print("positive")
+
+#3rd program
+num = int(input())
+if num%2 == 0:
+    print("Even")
+else:
+    print("Odd")
+
+#4th program
+age = int(input())
+if age>=18:
+    print("Eligible for vote")
+else:
+    print("Not Eligible")
+
+#5th program
+num = int(input())
+if num%3 == 0:
+    print("Divisible by 3")
+else:
+    print("Not Divisible by 3")
+
+#6th program
+num = int(input())
+if num%3 == 0 and num%5 == 0:
+    print("Divisible by 3 and 5")
+else:
+    print("Not Divisible by 3 and 5")
+
+#7th program
+a=40
+b=20
+if a>b:
+    print(a)
+else:
+    print(b)
